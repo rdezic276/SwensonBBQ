6c0135c99dc899b13bcbe45581c0772ff1a7102b
SC\rasim.dezic <rasim.dezic@my.simpson.edu>
2016-11-01 12:37:04 -0500

    All pages have been changed from html to php and have code added where the navigation code used to be.

---
+++ public_html/location.php
@@ -0,0 +1,33 @@
+<!-- Trey Moddelmog -->
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset= "utf-8" >
+  <link rel="stylesheet" type="text/css" href="css/css_stylesheet.css">
+  <title>Location and Hours</title>
+</head>
+
+<body>
+
+<?php include '../resources/navigation.php'; ?>
+
+<h1>Business Hours </h1>
+<p class= "odd"> Monday - Friday: 8AM - 5PM </p>
+<p class = "even"> Saturday: 8AM - 12PM </p>
+<p class= "odd"> Sunday: Closed </p><br><br>
+
+<h1>Address</h1>
+<p> 111 Drive Ave. Blanktown, IA 54321 </p>
+
+<div><img src="img/map.png" alt="map" id= "map"/></div>
+
+<footer>
+2016 SwensonBBQ, All Rights Reserved.
+</footer>
+
+</body>
+
+</html>
--- public_html/login.php
+++ public_html/login.php
@@ -12,42 +12,7 @@
 
 <body>
 
-<nav>
-    <ul>
-      <li>
-        <a href="index.html">Home</a>
-      </li>
-
-      <li>
-        <a href="about.html">About Us</a>
-      </li>
-      
-      <li>
-        <a href="info.html">Information</a>
-      </li>
-
-      <li>
-        <a href="location.html">Location and Business Hours</a>
-      </li>
-
-      <li>
-        <a href="item.html">Items For Sale</a>
-      </li>
-      
-      <li>
-        <a href="news.html">The BBQ Tribune</a>
-      </li>
-    
-      <li>
-        <a href="signup.html">Sign Up Page</a>
-      </li>
-      
-      <li>
-        <a href="login.html">Login</a>
-      </li>
-     
-    </ul>
-</nav>
+<?php include '../resources/navigation.php'; ?>
 
 <h1> Login!</h1>
 <div><img src="img/graphic.png" alt="graphic" /></div>
